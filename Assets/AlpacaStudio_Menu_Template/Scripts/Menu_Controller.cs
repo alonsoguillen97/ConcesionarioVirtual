a0cdbb4a1519afcafbbbf6d59559f430c2d56027
Alonso Guillén Domínguez <alonsobarbate1@gmail.com>
2021-05-18 22:11:36 +0200

    Proyecto base

---
+++ Assets/AlpacaStudio_Menu_Template/Scripts/Menu_Controller.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu_Controller : MonoBehaviour {
+
+	[Tooltip("_sceneToLoadOnPlay is the name of the scene that will be loaded when users click play")]
+	public string _sceneToLoadOnPlay = "Level";
+	[Tooltip("_webpageURL defines the URL that will be opened when users click on your branding icon")]
+	public string _webpageURL = "http://www.alpaca.studio";
+	[Tooltip("_soundButtons define the SoundOn[0] and SoundOff[1] Button objects.")]
+	public Button[] _soundButtons;
+	[Tooltip("_audioClip defines the audio to be played on button click.")]
+	public AudioClip _audioClip;
+	[Tooltip("_audioSource defines the Audio Source component in this scene.")]
+	public AudioSource _audioSource;
+
+
+    public Button Lambo;
+    public Button Porsche;
+    public Button Mercedes;
+    public Button Personalizado;
+
+    //The private variable 'scene' defined below is used for example/development purposes.
+    //It is used in correlation with the Escape_Menu script to return to last scene on key press.
+    UnityEngine.SceneManagement.Scene scene;
+
+	void Awake () {
+		if(!PlayerPrefs.HasKey("_Mute")){
+			PlayerPrefs.SetInt("_Mute", 0);
+		}
+
+        Lambo.GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("Lamborghini"));
+        Porsche.GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("Porsche"));
+        Mercedes.GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("Mercedes"));
+        Personalizado.GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("Personalizado"));
+    }
+	
+	public void OpenWebpage () {
+		_audioSource.PlayOneShot(_audioClip);
+		Application.OpenURL(_webpageURL);
+	}
+	
+	
+	public void Mute () {
+		_audioSource.PlayOneShot(_audioClip);
+		_soundButtons[0].interactable = true;
+		_soundButtons[1].interactable = false;
+		PlayerPrefs.SetInt("_Mute", 1);
+	}
+	
+	public void Unmute () {
+		_audioSource.PlayOneShot(_audioClip);
+		_soundButtons[0].interactable = false;
+		_soundButtons[1].interactable = true;
+		PlayerPrefs.SetInt("_Mute", 0);
+	}
+	
+	public void QuitGame () {
+		_audioSource.PlayOneShot(_audioClip);
+		#if !UNITY_EDITOR
+			Application.Quit();
+		#endif
+		
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+	}
+}
---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Quit : MonoBehaviour
+{
+    public Button menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("Standalone"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
